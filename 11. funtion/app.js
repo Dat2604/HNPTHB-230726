068468846e26516efc5b4ce68b985248b79f89f1
Dat1998 <dat0969558704@gmail.com>
2023-08-27 21:53:46 +0700

    baitap

--- 11. funtion/app.js
+++ 11. funtion/app.js
@@ -34,7 +34,70 @@
 //  }
 //  console.log(arrayprime);
 
- 
- 
-// function đàn cừu
+
+//  FUNTION REACTION CON CỪU
+// function findmaxsheep(sheeps){
+//     let maxsheep = sheeps [0]
+//     for ( let i = 1 ; i < sheeps.length ; i++){
+//         const sheep = sheeps [i] ;
+//         if ( sheep> maxsheep){
+//             maxsheep = sheep
+//         }
+//     }
+//     console.log(`con cừu lớn nhất của tôi có kich thước ${maxsheep}, hãy thién nó`);
+
+//     return maxsheep
+// }
+
+// const resetsheepsbymaxsheep = (maxsheep , sheeps)=>{
+//     const indexmaxsheep = sheeps.indexof (maxsheep);
+//     let newsheeps = sheeps.map((sheep,index)=>{
+//         if (index == indexmaxsheep){
+//             return 8
+//         }
+//         else{
+//             return sheep
+//         }
+//     })
+//     console.log("sau khi thiến đây là đàn cừu của tôi :" , sheeps);
+//     return newsheeps
+// }
+
+// const updatesheeps50kg = function(sheeps){
+//     let sheepaftermonth = []
+//     for ( let i = 0 ; i< sheeps.length ; i++){
+//         const sheep = sheeps [i];
+//         let newsheep = sheep + 50 ;
+//         sheepaftermonth.push (newsheep)
+//     }
+//     console.log(" đã đc 1 tháng trôi qua ,bầy cừu của tôi đã lớn,và đây là kích thước của nó :",sheeps);
+
+//     return sheepaftermonth
+
+// }
+
+// // let sheeps = [5,7,300,90,24,50,75]
+// // console.log("xin chào đây là đàn cừu của tôi :", sheeps);
+// // let maxsheep = findmaxsheep (sheeps)
+// // let newsheep1 = resetsheepsbymaxsheep (maxsheep,sheeps)
+// // let newsheepupdate = updatesheeps50kg ( newsheep1)
+
+// // let maxsheep1 = findmaxsheep(newsheepupdate)
+// // let newsheep2 = resetsheepsbymaxsheep (maxsheep1,newsheepupdate)
+// // let newsheepupdate1 = updatesheeps50kg ( newsheep2)
+
+
+// for (let i = 0 ; i<3 ; i++){
+//     console.log(` MONTH ${ i + 1}`);
+//     sheeps = findmaxsheep (sheeps)
+//     sheeps = resetsheepsbymaxsheep (maxsheep,sheeps)
+//     sheeps = updatesheeps50kg ( newsheep1)
+// }
+// const sum = sheeps.reduce((total ,sheep)=>{
+//     total = total+ sheep
+//     return total
+
+// }, 0)
+// console.log("", sum *2)
+
 
--- 12.thuc hanh/app.js
+++ 12.thuc hanh/app.js
@@ -1,62 +1,172 @@
-// bài 1
-// phần foot
-function foottometer(foot){
-   return foot * 0.305
-}
-let meter = foottometer()
-// phần 2
-function mettofeet(meter){
-    return meter * 3.279
-}
-let foot = mettofeet ()
+// // bài 1
+// // phần foot
+// function foottometer(foot){
+//    return foot * 0.305
+// }
+// let meter = foottometer()
+// // phần 2
+// function mettofeet(meter){
+//     return meter * 3.279
+// }
+// let foot = mettofeet ()
 
-//  bài 2
-let wellcome = ()=>{
-    console.log("Xin chào Rikkei Academy") ;
-}
-wellcome()
+// //  bài 2
+// let wellcome = ()=>{
+//     console.log("Xin chào Rikkei Academy") ;
+// }
+// wellcome()
 
-//  bài 3
-let square = (number) =>{
-    return number * number
-}
-square()
+// //  bài 3
+// let square = (number) =>{
+//     return number * number
+// }
+// square()
 
-// bài 4
-const  centuryfromyear = (year) =>{
-    const century = year / 100
-    return Math.ceil (century)
-}
-let century = centuryfromyear()
-console.log("", century)
+// // bài 4
+// const  centuryfromyear = (year) =>{
+//     const century = year / 100
+//     return Math.ceil (century)
+// }
+// let century = centuryfromyear()
+// console.log("", century)
 
-// bài 5
-const splitstring = ( string) =>{
-    let lengthstring =string.length ;
-    if (lengthstring < 15){
-        return string
-    }
-    const splitstr = string.slice (0 ,15)
-    return splitstr + "..."
-}
-const string = splitstring()
+// // bài 5
+// const splitstring = ( string) =>{
+//     let lengthstring =string.length ;
+//     if (lengthstring < 15){
+//         return string
+//     }
+//     const splitstr = string.slice (0 ,15)
+//     return splitstr + "..."
+// }
+// const string = splitstring()
 
 
-// bài 6
-function firstchauppercase (string){
-    let firstchar = string [0].touppercase();
-    let speedstring = string.slice(1).tolowercase()
-    return firstchar + speedstring ;
-}
-const result = firstchauppercase()
+// // bài 6
+// function firstchauppercase (string){
+//     let firstchar = string [0].touppercase();
+//     let speedstring = string.slice(1).tolowercase()
+//     return firstchar + speedstring ;
+// }
+// const result = firstchauppercase()
 
-//  bài 7
-function findmax(arraynumber){
-    let max = arraynumber [0];
-    for ( let i = 0 ; i< arraynumber.length ; i++){
-        const number = arraynumber [i];
-        if ( number > max ){
-            max = number
+// //  bài 7
+// function findmax(arraynumber){
+//     let max = arraynumber [0];
+//     for ( let i = 0 ; i< arraynumber.length ; i++){
+//         const number = arraynumber [i];
+//         if ( number > max ){
+//             max = number
+//         }
+//     }
+//     return max
+// }
+// let maxnumber = findmax([])
+// console.log("", maxnumber)
+
+
+// //  bài 8
+// // tìm số chẵn lẻ
+// function lachanle (number) {
+//     if (number % 2==0) {
+//         return `${number} là số chẵn`
+//     }
+//         return ` ${number} là số lẻ`
+// }
+// let numberstr = lachanle()
+// console.log("",numberstr)
+
+// // tim số  nguyên tố
+// function lasonguyento(number){
+//     let songuyento = true
+//     if (number < 2 ){
+//         return `${number} không phải là số nguyên tố`
+//     }
+//     else{
+//         for( let i = 2 ; i< number ;i++){
+//             if(number % i== 0){
+//                 songuyento = false
+//                 break;
+//             }
+//         }
+//     }
+//     if (songuyento){
+//         return `${number}là số nguyên tố`
+//     }
+//     return`${number} không phải số nguyên tố`
+// }
+// console.log(lasonguyento());
+
+// //  tìm số hoàn hảo
+// const lasohoanhao = (number) =>{
+//     let uoccuanumber = []
+//     for (let i = 0 ; i< number ; i++){
+//         if (number % i == 0){
+//             uoccuanumber.push(i)
+//         }
+//     }
+//     let sum = 0
+//     for( let i = 0 ; i< uoccuanumber.length; i++){
+//         const uoc = uoccuanumber[i];
+//         sum =+ uoc
+//     }
+//     if(number == sum){
+//         return`${number} là số hoàn hảo`
+//     }
+//         return `${number} ko phải là số hoàn hảo`   
+// }
+// let sohoanhao = lasohoanhao()
+// console.log("" , sohoanhao)
+
+// // kiểm tra toàn bộ
+// function main( number){
+//     let checkchanle = lachanle (number)
+//     let checksonguyento = lasonguyento (number)
+//     let checkhoanhao = lasohoanhao (number)
+// }
+// let kiemtra = main()
+// console.log("",kiemtra)
+
+
+// //  bài 9
+// function average (arraynumber){
+//     let sumarray = 0 
+//     for ( let i = 0 ;i< arraynumber.length; i++){
+//         const number = arraynumber[i];
+//         sumarray =+ number
+//     }
+//     return sumarray / arraynumber.length
+// }
+// let trungbinhcong = average([])
+// console.log ("", trungbinhcong)
+
+
+// //  bài 10
+// function logsonguyento ( number){
+//     for( let i = 0 ; i< number ; i++){
+//         const checknumber = lasonguyento (i)
+//         if(checknumber){
+//             console.log(i);
+//         }
+//     }
+// }
+// let nhapvao = logsonguyento ()
+// console.log("" , nhapvao)
+
+// baì tập về nhà 
+// bài 1 : sắp xếp  các phần tử trong mảng theo thứ tự từ bé đến lớn
+     function mindenmax (array) {
+       let arrayNumber = array;
+       arrayNumber.sort((a,b) => {return a - b})
+       console.log(arrayNumber); 
         }
-    }
-}
+        mindenmax([25,55,4,45,14,36])
+
+// bài 2 : sắp xếp các phần tử trong mảng theo thứ tự từ lớn đến bé
+        function maxdenmin (array) {
+            let arrayNumber = array;
+            arrayNumber.sort((a,b) => {return b - a})
+            console.log(arrayNumber); 
+             }
+             maxdenmin([44,4,50,98,26,4])
+     
